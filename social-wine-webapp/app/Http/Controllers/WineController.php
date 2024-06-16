1cea61c275fd0e2fcb846f8b92b603daa86a4d61
reecoo <f.serra@contrader.it>
2024-06-16 17:21:33 +0200

    aggiornato WineController

--- social-wine-webapp/app/Http/Controllers/WineController.php
+++ social-wine-webapp/app/Http/Controllers/WineController.php
@@ -14,7 +14,8 @@ class WineController extends Controller
      */
     public function index()
     {
-        //
+        $wine = Wine::all();
+        return response(view('wine.index', compact('wine')));
     }
 
     /**
@@ -24,18 +25,34 @@ class WineController extends Controller
      */
     public function create()
     {
-        //
+        return response(view('wine.create'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * 
      */
     public function store(Request $request)
     {
-        //
+        $request -> validate ([
+            'name',
+            'uvaggio',
+            'annata',
+            'descrizione',
+            'categoria',
+            'storia',
+            'image_path',
+            'prezzo',
+            'listino',
+            'cantina_id',
+        ]);
+
+        Wine::create($request -> all());
+
+        return redirect() -> route('wine.index')
+                          -> with ('success', 'Vino inserito con successo.');
     }
 
     /**
@@ -46,7 +63,8 @@ class WineController extends Controller
      */
     public function show(Wine $wine)
     {
-        //
+        $wine = Wine::findOrFail($wine);
+        return response(view('wine.show', compact('wine')));
     }
 
     /**
@@ -57,7 +75,8 @@ class WineController extends Controller
      */
     public function edit(Wine $wine)
     {
-        //
+        $wine = Wine::findOrFail($wine);
+        return response(view('wine.edit', compact('wine')));
     }
 
     /**
@@ -65,21 +84,42 @@ class WineController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Wine  $wine
-     * @return \Illuminate\Http\Response
+     * 
      */
     public function update(Request $request, Wine $wine)
     {
-        //
+        $request -> validate ([
+            'name',
+            'uvaggio',
+            'annata',
+            'descrizione',
+            'categoria',
+            'storia',
+            'image_path',
+            'prezzo',
+            'listino',
+            'cantina_id',
+        ]);
+
+        $wine = Wine::findrOrFail($wine);
+        $wine -> update($request -> all());
+
+        return redirect() -> route ('wine.index')
+                          -> with ('success', 'Vino modificato con successo.');
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Wine  $wine
-     * @return \Illuminate\Http\Response
+     * 
      */
     public function destroy(Wine $wine)
     {
-        //
+        $wine = Wine::findOrFail($wine);
+        $wine -> delete();
+
+        return redirect() -> route('wine.index')
+                          -> with ('success', 'Vino eliminato con successo.');
     }
 }
