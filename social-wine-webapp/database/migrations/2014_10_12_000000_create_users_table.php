1c1b239558869bd61f8a723b3553a0229bc208fa
reecoo <f.serra@contrader.it>
2024-06-16 23:40:47 +0200

    added enum in migration

--- social-wine-webapp/database/migrations/2014_10_12_000000_create_users_table.php
+++ social-wine-webapp/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table-> enum("role", ['seller', 'winery', 'user', 'admin']);
             $table->string('name');
             $table->string('surname')->nullable();
             $table->string('email')->unique();
